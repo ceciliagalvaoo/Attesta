86a3baf26267cc2ed80539f727765e405f7d2d1c
Cecília Galvão <Cecilia.Galvao@sou.inteli.edu.br>
2026-08-22 13:19:13 -0300

    chore(contract): lockfile and package entrypoint missed in the earlier split

---
+++ contract/src/index.ts
@@ -0,0 +1,32 @@
+// This file is part of Attesta.
+// Copyright (C) 2026 The Attesta Contributors
+// SPDX-License-Identifier: Apache-2.0
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { CompiledContract } from "@midnight-ntwrk/midnight-js-protocol/compact-js";
+
+export * from "./managed/attesta/contract/index.js";
+export * from "./witnesses";
+
+import * as CompiledAttestaContract from "./managed/attesta/contract/index.js";
+import * as Witnesses from "./witnesses";
+
+export const CompiledAttestaContractContract = CompiledContract.make<
+  CompiledAttestaContract.Contract<Witnesses.AttestaPrivateState>
+>(
+  "Attesta",
+  CompiledAttestaContract.Contract<Witnesses.AttestaPrivateState>,
+).pipe(
+  CompiledContract.withWitnesses(Witnesses.witnesses),
+  CompiledContract.withCompiledFileAssets("./managed/attesta"),
+);
